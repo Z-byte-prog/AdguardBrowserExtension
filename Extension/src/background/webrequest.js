ce9eeb12af12ec618791b537e3f6c2ca3f8030ab
mizzick <ds.kolyshev@gmail.com>
2021-09-28 11:32:44 +0200

    fix:AG-10246 fix ContentFiltering

--- Extension/src/background/webrequest.js
+++ Extension/src/background/webrequest.js
@@ -209,6 +209,35 @@ const webrequestInit = function () {
             collapseElement(tabId, requestFrameId, requestUrl, referrerUrl, requestType);
         }
 
+        // Content filtering will be undefined for chromium based builds
+        if (contentFiltering) {
+            const replaceRules = webRequestService.getReplaceRules(tab, requestUrl, referrerUrl, requestType) || [];
+            const htmlRules = webRequestService.getContentRules(tab, referrerUrl) || [];
+
+            if (replaceRules.length > 0 || htmlRules.length > 0) {
+                // TODO: We don't have it yet
+                // const contentType = browserUtils.getHeaderValueByName(responseHeaders, 'content-type');
+                const contentType = 'text/html; charset=utf-8';
+
+                const request = new TSUrlFilter.Request(
+                    requestUrl, referrerUrl, RequestTypes.transformRequestType(requestType),
+                );
+                request.requestId = requestId;
+                request.tabId = tab.tabId;
+                // TODO: We don't have it yet
+                request.statusCode = 200;
+                request.method = method;
+
+                contentFiltering.apply(
+                    backgroundPage.webRequest.filterResponseData(requestId),
+                    request,
+                    contentType,
+                    replaceRules || [],
+                    htmlRules || [],
+                );
+            }
+        }
+
         return response;
     }
 
@@ -360,7 +389,6 @@ const webrequestInit = function () {
         const referrerUrl = getReferrerUrl(requestDetails);
         const { requestId } = requestDetails;
         const { statusCode } = requestDetails;
-        const { method } = requestDetails;
 
         requestContextStorage.update(requestId, { responseHeaders });
 
@@ -374,32 +402,6 @@ const webrequestInit = function () {
             // Do not await function bellow, otherwise csp rules won't apply in time
             // Issue AG-6230
             filterSafebrowsing(tab, requestUrl);
-        }
-
-        // Content filtering will be undefined for chromium based builds
-        if (contentFiltering) {
-            const replaceRules = webRequestService.getReplaceRules(tab, requestUrl, referrerUrl, requestType) || [];
-            const htmlRules = webRequestService.getContentRules(tab, referrerUrl) || [];
-
-            if (replaceRules.length > 0 || htmlRules.length > 0) {
-                const contentType = browserUtils.getHeaderValueByName(responseHeaders, 'content-type');
-
-                const request = new TSUrlFilter.Request(
-                    requestUrl, referrerUrl, RequestTypes.transformRequestType(requestType),
-                );
-                request.requestId = requestId;
-                request.tabId = tab.tabId;
-                request.statusCode = statusCode;
-                request.method = method;
-
-                contentFiltering.apply(
-                    backgroundPage.webRequest.filterResponseData(requestId),
-                    request,
-                    contentType,
-                    replaceRules || [],
-                    htmlRules || [],
-                );
-            }
         }
 
         let responseHeadersModified = false;
